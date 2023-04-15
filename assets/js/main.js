643138a041def6a3c8a887288e593e5b227142bb
Nadir123649 <nadir.novatore@gmail.com>
2023-04-15 03:35:49 -0700

    faqpage-new-changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,70 +7,69 @@ $('.testominal-slider').slick({
     cssEase: 'linear',
     prevArrow: '<button type="button" class="slick-prev"><i class="icon-long-arrow-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="icon-long-arrow-right"></i></button>',
-    responsive: [
-      {
-        breakpoint: 450,
-        settings: {
-          dots: false,
-          slidesToShow: 3,
-          centerPadding: '0px',
+    responsive: [{
+            breakpoint: 450,
+            settings: {
+                dots: false,
+                slidesToShow: 3,
+                centerPadding: '0px',
+            }
+        },
+        {
+            breakpoint: 420,
+            settings: {
+                autoplay: true,
+                dots: false,
+                slidesToShow: 1,
+                centerMode: false,
+            }
         }
-      },
-      {
-        breakpoint: 420,
-        settings: {
-          autoplay: true,
-          dots: false,
-          slidesToShow: 1,
-          centerMode: false,
-        }
-      }
     ]
-  });
+});
 
 //   sliders
-$(document).ready(function () {
+$(document).ready(function() {
     $('.portfolio-slider').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.slick-carousel'
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slick-carousel'
     });
     $('.slick-carousel').slick({
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      asNavFor: '.portfolio-slider',
-      dots: false,
-      nav: true,
-      arrows:true,
-      prevArrow: '<button type="button" class="slick-prev"><i class="icon-long-arrow-left"></i></button>',
-      nextArrow: '<button type="button" class="slick-next"><i class="icon-long-arrow-right"></i></button>',
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        asNavFor: '.portfolio-slider',
+        dots: false,
+        nav: true,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="icon-long-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="icon-long-arrow-right"></i></button>',
     });
-  });
+});
 
 //   tabs js
 const tabButtons = document.querySelectorAll('.tab-button');
 const tabContents = document.querySelectorAll('.tab-content');
 
 tabButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const tab = button.dataset.tab;
+    button.addEventListener('click', () => {
+        const tab = button.dataset.tab;
 
-    // Hide all tab contents
-    tabContents.forEach(content => {
-      content.classList.add('hidden');
+        // Hide all tab contents
+        tabContents.forEach(content => {
+            content.classList.add('hidden');
+        });
+
+        // Show the selected tab content
+        document.getElementById(tab).classList.remove('hidden');
+
+        // Set active class on the clicked button
+        tabButtons.forEach(button => {
+            button.classList.remove('active');
+        });
+        button.classList.add('active');
     });
-
-    // Show the selected tab content
-    document.getElementById(tab).classList.remove('hidden');
-
-    // Set active class on the clicked button
-    tabButtons.forEach(button => {
-      button.classList.remove('active');
-    });
-    button.classList.add('active');
-  });
 });
 
 // accordion js  for opening
@@ -78,25 +77,19 @@ tabButtons.forEach(button => {
 const accordionItems = document.querySelectorAll('.accordion-item');
 
 accordionItems.forEach(item => {
-  const header = item.querySelector('.accordion-header');
-  const content = item.querySelector('.accordion-content');
-  const arrow = header.querySelector('.arrow');
+    const header = item.querySelector('.accordion-header');
+    const content = item.querySelector('.accordion-content');
+    const arrow = header.querySelector('.arrow');
 
-  header.addEventListener('click', () => {
-    item.classList.toggle('active');
+    header.addEventListener('click', () => {
+        item.classList.toggle('active');
 
-    if (item.classList.contains('active')) {
-      content.style.display = 'block';
-      arrow.classList.add('up');
-    } else {
-      content.style.display = 'none';
-      arrow.classList.remove('up');
-    }
-  });
+        if (item.classList.contains('active')) {
+            content.style.display = 'block';
+            arrow.classList.add('up');
+        } else {
+            content.style.display = 'none';
+            arrow.classList.remove('up');
+        }
+    });
 });
-
-
-
-
-
-
